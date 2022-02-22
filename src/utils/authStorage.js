da4c2b0e01973fc8b712ec46815d3a543856f3d0
Ken Holston <git.kholston@gmail.com>
2022-02-22 02:19:37 -0500

    exercise 10.14

---
+++ src/utils/authStorage.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    const rawToken = await AsyncStorage.getItem(
+      `${this.namespace}:accessToken`
+    )
+    return rawToken ? rawToken : ''
+  }
+
+  async setAccessToken(accessToken) {
+    await AsyncStorage.setItem(
+      `${this.namespace}:accessToken`,
+        accessToken
+    )
+  }
+
+  async removeAccessToken() {
+    await AsyncStorage.removeItem(
+      `${this.namespace}:accessToken`
+    );
+  }
+}
+
+export default AuthStorage;
